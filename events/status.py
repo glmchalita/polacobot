b6795b6ec04f43a53d3182f9503eeb5fdff7f922
Chalitvv <guilhermechalita@hotmail.com>
2021-09-08 22:23:17 -0300

    refactor: remove warns

--- events/status.py
+++ events/status.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
 from asyncio import sleep
-from functions import client
 
 class Status(commands.Cog):
     def __init__(self, client):
         self.client = client
+        client.loop.create_task(self.random_status(client))
 
     @commands.Cog.listener()
-    async def random_status(self):
+    async def random_status(self, client):
         while True:
             await client.wait_until_ready()
             await client.change_presence(status=discord.Status.online, activity=discord.Game(name='Long Beach RP'))
@@ -17,7 +17,6 @@ class Status(commands.Cog):
             await sleep(8)
             await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name='DEV Chali#3955'))
             await sleep(8)
-    client.loop.create_task(random_status(client))
 
 def setup(client):
     client.add_cog(Status(client))
