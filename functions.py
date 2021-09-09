ed96a28aa4b758a18720df76a7d22d0844d8f1b7
Chalitvv <guilhermechalita@hotmail.com>
2021-09-08 22:57:54 -0300

    feat: add color function

--- functions.py
+++ functions.py
@@ -7,17 +7,21 @@ intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix='.', case_insensitive=True, intents=intents)
 
+def red(arg): return f'\033[91m{arg}\033[0m'
+def yellow(arg): return f'\033[93m{arg}\033[0m'
+def green(arg): return f'\033[32m{arg}\033[0m'
+
 def ensure_commands(extension):
     try:
         client.load_extension(f'commands.{extension}')
     except Exception as e:
-        print(f'\033[93mFail to load command {extension}:\033[0m \033[91m{e}\033[0m')
+        print(yellow(f'Fail to load command {extension}:'), red(e))
 
 def ensure_events(extension):
     try:
         client.load_extension(f'events.{extension}')
     except Exception as e:
-        print(f'\033[93mFail to load event {extension}:\033[0m \033[91m{e}\033[0m')
+        print(yellow(f'Fail to load command {extension}:'), red(e))
 
 def read_token():
     with open("token.txt", "r") as f:
@@ -28,3 +32,4 @@ def is_owner():
     async def predicate(ctx):
         return ctx.author.id == 330102349425016842
     return commands.check(predicate)
+
