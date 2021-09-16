034e2e0490483980c85d106d35eb196e22d2f02e
Guilherme Chalita <guilhermehchalita@gmail.com>
2021-09-16 14:16:13 -0300

    rem: remove line function

--- events/error.py
+++ events/error.py
@@ -1,10 +1,5 @@
-import sys
 from discord.ext import commands
 from functions import red
-
-def line():
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    return exc_tb.tb_lineno
 
 class Error(commands.Cog):
     def __init__(self, client):
